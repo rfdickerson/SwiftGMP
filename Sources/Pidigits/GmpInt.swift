116acab42d187dbb4548a3f266a92644ba0b0ce0
Robert F. Dickerson <rfdickerson@gmail.com>
2016-07-05 14:10:52 -0500

    Gmp Swift wrapper

--- Sources/Pidigits/GmpInt.swift
+++ Sources/Pidigits/GmpInt.swift
@@ -1,9 +1,94 @@
-//
-//  GmpInt.swift
-//  pidigits
-//
-//  Created by Robert F. Dickerson on 7/4/16.
-//
-//
-
 import Foundation
+import SwiftGmp
+
+class GmpInt {
+    
+    var i = mpz_t()
+    var dummy = 0
+    
+    init(_ value: Int = 0) {
+        
+        __gmpz_init(&i)
+        __gmpz_set_si(&i, value )
+        
+    }
+    
+    init(withPointer pointer: UnsafeMutablePointer<mpz_t>) {
+        self.i = pointer.pointee
+    }
+    
+    deinit {
+        __gmpz_clear(&i)
+    }
+    
+    var intValue: Int {
+        
+        let ret = __gmpz_get_si(&self.i)
+        return ret
+        
+    }
+    
+}
+
+extension GmpInt: Equatable {}
+
+extension GmpInt: Comparable {}
+
+func * (lhs: GmpInt, rhs: GmpInt) -> GmpInt {
+    let ret = GmpInt()
+    __gmpz_mul(&ret.i, &lhs.i, &rhs.i)
+    return ret
+}
+
+func * (lhs: Int, rhs: GmpInt) -> GmpInt {
+    let ret = GmpInt()
+    let tmp = GmpInt(lhs)
+    __gmpz_mul(&ret.i, &tmp.i, &rhs.i)
+    return ret
+}
+
+func + (lhs: GmpInt, rhs: GmpInt) -> GmpInt {
+    let ret = GmpInt()
+    __gmpz_add(&ret.i, &lhs.i, &rhs.i)
+    return ret
+}
+
+func += (lhs: inout GmpInt, rhs: GmpInt) {
+    __gmpz_add(&lhs.i, &lhs.i, &rhs.i)
+}
+
+func += (lhs: inout GmpInt, rhs: Int) {
+    let tmp = GmpInt(rhs)
+    __gmpz_add(&lhs.i, &lhs.i, &tmp.i)
+}
+
+func *= (lhs: inout GmpInt, rhs: GmpInt) {
+    __gmpz_mul(&lhs.i, &lhs.i, &rhs.i)
+}
+
+func == (lhs: GmpInt, rhs: GmpInt) -> Bool {
+    return (0 == __gmpz_cmp(&lhs.i, &rhs.i))
+}
+
+
+func != (lhs: GmpInt, rhs: GmpInt) -> Bool {
+    return !(lhs == rhs)
+}
+
+func / (lhs: GmpInt, rhs: GmpInt) -> GmpInt {
+    let ret = GmpInt(0)
+    __gmpz_fdiv_q(&ret.i,&lhs.i,&rhs.i)
+    return ret
+}
+
+func /= (lhs: GmpInt, rhs: GmpInt) {
+    __gmpz_fdiv_q(&lhs.i, &lhs.i, &rhs.i)
+}
+
+//func <= (lhs: GmpInt, rhs: GmpInt) -> Bool {
+//    return (__gmpz_cmp(&lhs.i, &rhs.i) < 0)
+//}
+
+func <(lhs: GmpInt, rhs: GmpInt) -> Bool {
+    return (__gmpz_cmp(&lhs.i, &rhs.i) < 0)
+}

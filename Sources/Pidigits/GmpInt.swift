c3206968e4c45d1d8cfe73532a2689e2e9bfa8f6
Robert F. Dickerson <rfdickerson@gmail.com>
2016-07-05 14:10:26 -0500

    Add tests

---
+++ Sources/Pidigits/GmpInt.swift
@@ -0,0 +1,9 @@
+//
+//  GmpInt.swift
+//  pidigits
+//
+//  Created by Robert F. Dickerson on 7/4/16.
+//
+//
+
+import Foundation
--- Tests/Pidigits/pidigitsTests.swift
+++ Tests/Pidigits/pidigitsTests.swift
@@ -3,15 +3,101 @@ import XCTest
 
 class pidigitsTests: XCTestCase {
     
-    func testExample() {
+    func testComputePi() {
         
-        computePi(withDigits: 15)
+        self.measure() {
+            computePi(withDigits: GmpInt(25))
+        }
         
+    }
+    
+    func testMultiply() {
+        let a = GmpInt(6)
+        let b = GmpInt(7)
+        
+        let c = a * b
+        
+        let i = c.intValue
+        
+        XCTAssertEqual(i, 42)
+        
+    }
+    
+    func testAdd() {
+        let a = GmpInt(6)
+        let b = GmpInt(7)
+        
+        let c = a + b
+        
+        let i = c.intValue
+        
+        XCTAssertEqual(i, 13)
+    }
+    
+    func testAddMult() {
+        let a = GmpInt(6)
+        let b = GmpInt(7)
+        let c = GmpInt(3)
+        
+        let d = a * b + c
+        
+        let i = d.intValue
+        
+        XCTAssertEqual(i, 45)
+    }
+    
+    func testDivide() {
+        
+        let a = GmpInt(36)
+        let b = GmpInt(5)
+        
+        let c = a/b
+        
+        let i = c.intValue
+        
+        XCTAssertEqual(i, 7)
+        
+    }
+    
+    
+    func testIncrement() {
+        var a = GmpInt(6)
+        a += 1
+        
+        XCTAssertEqual(a.intValue, 7)
+    }
+    
+    func testEqual() {
+        let a = GmpInt(36)
+        let b = GmpInt(36)
+        let c = GmpInt(5)
+        
+        XCTAssertEqual(a, b)
+        XCTAssertNotEqual(a, c)
+    }
+    
+    func testLessThanEqual() {
+        
+        let a = GmpInt(36)
+        let b = GmpInt(5)
+        let c = GmpInt(36)
+        
+        XCTAssertLessThan(b, a)
+        XCTAssertLessThanOrEqual(a, c)
+        
+    }
+    
+    func testNegative() {
+        let a = GmpInt(-10)
+        let b = GmpInt(5)
+        
+        let c = a*b
+        XCTAssertEqual(c.intValue, -50)
     }
 
     static var allTests : [(String, (pidigitsTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testComputePi", testComputePi),
         ]
     }
 }
